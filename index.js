1e69957a81ee11e7da9633f296def09cbd9ae87c
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2018-06-07 10:22:52 +0300

    Add Tools and uuid function

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import "./http/fetch";
 import {Ruth} from "./core";
 import {Routing} from "./routing";
 import {i18n} from "./i18n";
-
+import {Tools} from "./tools";
 
 // Expose navigation method in Ruth object to be used as a shortcut for templates.
 Ruth.navigate = url => () => Routing.navigate(url);
+
+// Expose helper function in Ruth object
+Object.assign(Ruth, Tools);
 
 Ruth.addComponent(i18n);
 Ruth.initComponents();
