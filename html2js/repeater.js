570a1d9e4872da1ba5a4a5efc4fafa79850087b3
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2019-03-04 12:44:02 +0200

    Add index for repeater

--- html2js/repeater.js
+++ html2js/repeater.js
@@ -16,13 +16,13 @@ function addRepeaterNodeType(ast) {
 function repeaterGenerator(astNode, varName, parentNodeVarName, variables, level) {
     const prefix = [], suffix = [];
     const pad = " ".repeat(4 * level);
-    const iteratorsNames = {};
+    const iteratorsNames = {"by": "index"};
 
     astNode.attributes.forEach(({key, value}) => {
         iteratorsNames[key] = value;
     });
 
-    prefix.push(pad + `for(let ${iteratorsNames["as"]} of ${iteratorsNames["every"]}) {`);
+    prefix.push(pad + `for(const [${iteratorsNames["by"]}, ${iteratorsNames["as"]}] of ${iteratorsNames["every"]}.entries()) {`);
     suffix.push(pad + `}`);
 
     return {prefix, suffix, domNode: false};
